bc11d69764af3adcf0c8f07d5436f44df5fd0102
Rob Bos <rajbos@users.noreply.github.com>
2024-07-11 21:57:50 +0000

    cache the data

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,16 +1,21 @@
 import basename from "../../react-config";
 
+let cachedData = null;
+
 const getData = async () => {
-  try {
-	const response = await fetch(`${basename}data.json`);
-	if (!response.ok) {
-	  throw new Error('Network response was not ok');
-	}
-	return await response.json();
-  } catch (error) {
-	console.error('Fetch error:', error);
-	throw error;
-  }
+    if (cachedData) {
+        return cachedData;
+    }
+    try {
+        const response = await fetch(`${basename}data.json`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
+    }
 };
 
 export default getData;
