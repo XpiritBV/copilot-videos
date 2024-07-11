c04d2f532e87228c0e1ec59dea17ecf7aad21ea0
Rob Bos <rajbos@users.noreply.github.com>
2024-07-11 19:46:44 +0000

    testing

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // Determine the base path based on the environment
-const base = process.env.NODE_ENV === 'production' ? '' : '/';
+const base = process.env.NODE_ENV === 'production' ? '' : '';
 
 export default defineConfig({
   plugins: [react()],
   base: base,
+  build: {
+    outDir: 'dist',
+  },
 });
