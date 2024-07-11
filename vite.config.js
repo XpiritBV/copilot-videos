492e88cd2bf3e034c2bb8d5e6280833458b62590
Rob Bos <rajbos@users.noreply.github.com>
2024-07-11 19:20:32 +0000

    testing

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // Determine the base path based on the environment
-const base = process.env.NODE_ENV === 'production' ? '/copilot-videos/' : '/';
+const base = process.env.NODE_ENV === 'production' ? '' : '/';
 
 export default defineConfig({
   plugins: [react()],
