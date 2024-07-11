b93e304c1096e73497dd85ceb3523133cba86e5f
Rob Bos <rajbos@users.noreply.github.com>
2024-07-11 21:46:13 +0000

    test loading data

--- src/pages/Tutorials.jsx
+++ src/pages/Tutorials.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import '../styles.css';
+import basename from '../../react-config';
 
 const Tutorials = () => {
   const [tutorials, setTutorials] = useState([]);
 
   useEffect(() => {
-    fetch('data.json')
+    fetch(`${basename}data.json`)
       .then(response => response.json())
       .then(data => setTutorials(data.tutorials))
       .catch(error => console.error('Error loading tutorials:', error));
