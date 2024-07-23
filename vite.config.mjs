3df2968ea98c258aeb2f451c8b87dac1838d7a2b
Rob Bos <rajbos@users.noreply.github.com>
2024-07-23 18:51:32 +0000

    test

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import basename from './react-config';
 import rewriteAll from 'vite-plugin-rewrite-all';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [react(), rewriteAll()],
+  plugins: [
+    react(), 
+    rewriteAll(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          basePath: basename,
+        },
+      },      
+    }),
+  ],
   base: basename,
   build: {
     outDir: 'dist',
