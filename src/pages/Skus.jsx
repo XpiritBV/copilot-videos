12359b3b1bdfaa974382b9f003d686df25c361ee
Rob Bos <rajbos@users.noreply.github.com>
2024-10-10 20:21:14 +0000

    Testing with showing GHES support

--- src/pages/Skus.jsx
+++ src/pages/Skus.jsx
@@ -14,6 +14,23 @@ const Skus = () => {
     navigate(`/detail?videoId=${id}`);
   };
 
+  const handleGHESToggle = () => {
+    // hide the videos that do not have the ghes_support tag
+    const ghesToggle = document.getElementById('ghes-toggle');
+    const ghesSupport = document.getElementsByClassName('ghes-support');
+
+    if (ghesToggle.checked) {
+      for (let i = 0; i < ghesSupport.length; i++) {
+        ghesSupport[i].style.display = 'block';
+      }
+    }
+    else {
+      for (let i = 0; i < ghesSupport.length; i++) {
+        ghesSupport[i].style.display = 'none';
+      }
+    }
+  }
+
   useEffect(() => {
     getData()
       .then(data => {
@@ -27,6 +44,12 @@ const Skus = () => {
   return (
     <div>
       <Header title={`GitHub Copilot Business vs Enterprise`}/>
+      <button 
+          className={`toggle-button ${isOn ? 'on' : 'off'}`}
+          onClick={() => this.handleGHESToggle()}>
+        Only show GHES supported
+      </button>
+
       <div id="main-container">
         <div>
           <div>
@@ -35,9 +58,13 @@ const Skus = () => {
           <div id="business-features" className="sku-grid">
           {
             features.business.map(item => (
-              <div key={item.id} className="video-box" onClick={() => handleClick(item.id)}>
+              <div 
+                key={item.id} 
+                className={`video-box ${item.ghes_support ? 'ghes-support' : ''}`} 
+                onClick={() => handleClick(item.id)}
+              >
                 <h3>{item.title}</h3>
-                {!item.videoUrl && <div className="coming-soon-small">Coming soon</div>}
+                {!item.videoUrl && <div className="coming-soon-small">Video coming soon</div>}
               </div>
             ))
           }
