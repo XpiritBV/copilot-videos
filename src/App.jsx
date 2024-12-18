fd61aaee5c8f7e9301ecfaffcbe9b42f6de9dbf6
Rob Bos <rajbos@users.noreply.github.com>
2024-12-18 15:05:53 +0000

    Adding header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 import Index from './pages/Index';
 import Detail from './pages/Detail';
 import GameLandscape from './pages/GameLandscape';
@@ -12,18 +13,22 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router basename={basename}>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/gamelandscape" element={<GameLandscape />} />
-        <Route path="/gamelevel" element={<GameLevel />} />
-        <Route path="/skus" element={<Skus />} />
-        <Route path="/tutorials" element={<Tutorials />} /> 
-        {/* Add a catch-all route for 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <div>
+      <Header/>
+
+      <Router basename={basename}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/gamelandscape" element={<GameLandscape />} />
+          <Route path="/gamelevel" element={<GameLevel />} />
+          <Route path="/skus" element={<Skus />} />
+          <Route path="/tutorials" element={<Tutorials />} /> 
+          {/* Add a catch-all route for 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <script defer data-domain="github-copilot.xebia.ms" src="https://plausible.io/js/script.js"></script>
+    </header>
+  );
+}
+
+export default Header;
